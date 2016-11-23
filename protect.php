8a8488064e03205ab8f54024794d34f4e3b60d10
Ashwin Gupta <ashwingupta2000@gmail.com>
2016-11-23 15:04:19 -0800

    aproaching entropy...

--- protect.php
+++
@@ -1,9 +0,0 @@
-<?php
-	//echo "<p> hi </p>";
-	//var_dump($_POST);
-	if(strcmp($_POST["password"], "niwhsa9") == 0)  {
-		header("Location: http://68.101.98.197:8080/application.html");
-		exit;
-	}
-	else echo "<p>incorrect password</p>";
-?> 
