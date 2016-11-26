4659cdb02350c3109a5d304bfeb51979f8ab1c8d
Ashwin Gupta <ashwingupta2000@gmail.com>
2016-11-26 11:26:21 -0800

    fixed bug where php wasn't capturing stderr from javac, fixed broken <a> links from iframe

--- request-github.php
+++ request-github.php
@@ -1,6 +1,6 @@
 <?php
 	//phpinfo();
-	//if($_POST["key"] != key)...
+	if($_POST["key"] != "key") exit("<h1>fail</h1>"); 
 	echo "<link rel='stylesheet' type='text/css' href='style.css'>";
 	echo "<p>PHP file connected...</p>";
 	echo "User: ".get_current_user()." as: ".shell_exec("whoami");
@@ -18,23 +18,31 @@
 		1 => "sudo mkdir '$server_dir/uploads/extracted/$extract_dir'",
                 2 => "sudo unzip '$target_file' -d '$server_dir/uploads/extracted/$extract_dir'",
 		3 => "sudo ls '$server_dir/uploads/extracted/$extract_dir' -a", 
-		4 => "cd '$server_dir/uploads/extracted/$extract_dir'; ls -a;  sudo javac ".escapeshellarg($main)." -d '$server_dir/uploads/extracted/$extract_dir'", 
+		4 => "cd '$server_dir/uploads/extracted/$extract_dir'; ls -a;  sudo javac ".escapeshellarg($main)." -d '$server_dir/uploads/extracted/$extract_dir' 2>&1", 
 		5 => "cd '$server_dir/uploads/extracted/$extract_dir'; zip -r $server_dir/outputs/$extract_dir".".zip"." *" 
          );
 	/*check if inputs are null, dont run run()*/
+	$javacout = "";
+	if($main == NULL || $main = "")  exit("<p>Invalid arguments!</p>");
 	run($commands,$extract_dir);
 	function run($commands, $output_dir) {
-
+		global $javacout;
 		for($i = 0; $i < count($commands); $i++) {
 			echo "<p class ='cmd'><b> Running: $commands[$i]</b> </p>";
 			$res = shell_exec($commands[$i]);
 			echo "<p class='shellres'>$res</p>";
 			if($res == NULL) echo "<p class='shellres'>NULL</p>";
+			if($i == 4) {	 
+				 $javacout = $res;
+			}
 			echo "<hr class='breaks'>";
 		}
 		/* header("Location: http://68.101.98.197:8080/uploads"); */
-		echo "<script> function download() {window.open('http://68.101.98.197:8080/outputs/"."$output_dir".".zip');}</script>";
-		echo "<img src='fail' onerror='download()' style ='display:none;'/>";
+		echo "<script> function downloadzip() {window.open('http://68.101.98.197:8080/outputs/"."$output_dir".".zip'); }</script>";
+		echo "<img src='fail' onerror='downloadzip()' style ='display:none;'/>";
+		//header("Content-Type: text/plain");
+		//echo "<script>function download(filename,text){var element=document.createElement('a');element.setAttribute('href','data:text/plain;charset=utf-8,'+encodeURIComponent(text));element.setAttribute('download',filename);element.style.display='none';document.body.appendChild(element);element.click();document.body.removeChild(element)}</script>";
+		//echo "<img src='fail' style='display:none' onerror='download('log.txt', $javacout )'/>";
 	}
 ?>
 
